3bbe5afa35fbf3fdce238597e4fb917c29e3bc3f
qobilbek <ibrohimovqobilbek5@gmail.com>
2025-02-27 12:43:14 +0900

    feat: complete A-TASK

--- train.js
+++ train.js
@@ -3,15 +3,15 @@
 // function (letter, word) {
 
 // }
-console.log("Jack ma maslahatlari");
-const list = [
-  "yaxshi talaba bo'ling",
-  "tog'g'ri boshliq tanlang",
-  "o'zingizga ishlashni boshlang",
-  "siz kuchli bolgan narsalarni qiling",
-  "yoshlarga investitsia qiling",
-  "endi dam oling foydasi yo'q",
-];
+// console.log("Jack ma maslahatlari");
+// const list = [
+//   "yaxshi talaba bo'ling",
+//   "tog'g'ri boshliq tanlang",
+//   "o'zingizga ishlashni boshlang",
+//   "siz kuchli bolgan narsalarni qiling",
+//   "yoshlarga investitsia qiling",
+//   "endi dam oling foydasi yo'q",
+// ];
 
 // function maslahatBering(a, callback) {
 //   if (typeof a !== "number") callback("insert a number", null);
@@ -41,28 +41,28 @@ const list = [
 
 //ASYNCHRONIC FUNCTION
 
-async function maslahatBering(a) {
-  if (typeof a !== "number") throw new Error("iltimos raqam yozing");
-  else if (a <= 20) return list[0];
-  else if (a > 20 && a <= 30) return list[1];
-  else if (a > 30 && a <= 40) return list[2];
-  else if (a > 40 && a <= 50) return list[3];
-  else if (a > 50 && a <= 60) return list[4];
-  else {
-    return list[5];
+// async function maslahatBering(a) {
+//   if (typeof a !== "number") throw new Error("iltimos raqam yozing");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return list[5];
 
-    //     return new Promise((resolve, reject) => {
-    //       // promise function da set time out va set interval yaxshi ishlaydi//
-    //       setTimeout(() => {
-    //         resolve(list[5]);
-    //       }, 5000);
-    //     });
+//     //     return new Promise((resolve, reject) => {
+//     //       // promise function da set time out va set interval yaxshi ishlaydi//
+//     //       setTimeout(() => {
+//     //         resolve(list[5]);
+//     //       }, 5000);
+//     //     });
 
-    //  setTimeout(() => {
-    //    resolve(list[5]);
-    //  }, 5000);
-  }
-}
+//     //  setTimeout(() => {
+//     //    resolve(list[5]);
+//     //  }, 5000);
+//   }
+// }
 //SET INTERVAL
 // return new Promise((resolve, reject) => {
 //       setInterval(() => {
@@ -82,14 +82,20 @@ async function maslahatBering(a) {
 // console.log("passed here 1");
 
 //ASYN/AWAIT
-async function run() {
-  let javob = await maslahatBering(15);
-  console.log(javob);
-  javob = await maslahatBering(25);
-  console.log(javob);
-  javob = await maslahatBering(35);
-  console.log(javob);
-  javob = await maslahatBering(45);
-  console.log(javob);
+// async function run() {
+//   let javob = await maslahatBering(15);
+//   console.log(javob);
+//   javob = await maslahatBering(25);
+//   console.log(javob);
+//   javob = await maslahatBering(35);
+//   console.log(javob);
+//   javob = await maslahatBering(45);
+//   console.log(javob);
+// }
+// run();
+
+function countLetter(letter, word) {
+  return word.split(letter).length - 1;
 }
-run();
+
+console.log(countLetter("e", "engineere"));
